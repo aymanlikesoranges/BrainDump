e589ff4c675e5b41f223d8b14b2c3a82d0984040
achau08 <140908286+achau08@users.noreply.github.com>
2023-11-16 17:44:38 -0500

    Create entries.js

---
+++ calendar/entries.js
@@ -0,0 +1,12 @@
+const createBtn = document.querySelector(".btn");
+const notesContainer = document.querySelector(".notes-container");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
